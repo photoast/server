611fb2c7c666d6f83e56cb52d8fbb7db414cc792
jess <jaryapp@gmail.com>
2026-05-15 03:57:39 +0900

    카카오용 간편결제

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1011,7 +1011,7 @@ export default function GuestPage({ params }: { params: { slug: string } }) {
 
     window.AUTHNICE.requestPay({
       clientId: NICEPAY_CLIENT_ID,
-      method: 'card',
+      method: 'cartAndEasyPay',
       orderId,
       amount: paymentAmount,
       goodsName: `포토 프린트 ${printQuantity}매`,
